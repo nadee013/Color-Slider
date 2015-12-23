aff092c8833d9d2ba11393dabea867e5d780c1da
Nadee <nadee013@gmail.com>
2015-12-23 13:48:11 +0530

    Initial Commit

---
+++ Color Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Slider
+//
+//  Created by Nadee Anuththara on 12/23/15.
+//  Copyright © 2015 Nadee Anuththara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
